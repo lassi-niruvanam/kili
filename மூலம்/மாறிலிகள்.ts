122aba2c4f579039d1550df784d58cd3cff52591
julienmalard <julien.malard@mail.mcgill.ca>
2023-01-30 12:07:39 +0100

    முதல் வேலை

---
+++ மூலம்/மாறிலிகள்.ts
@@ -0,0 +1,3 @@
+export const பங்கேற்பாளர்_நெடுவரிசை_அடையாளம் = 'பங்கேற்பாளர்';
+export const பங்கேற்பாளர்_மாறி_அடையாளம் =
+  '/orbitdb/zdpuAqBsFnWsRo4DRZjyvDbuHXPck4PsD99GqUqnDaVMYYdRy/df20365f-e180-400f-b97c-945542c690f4';
