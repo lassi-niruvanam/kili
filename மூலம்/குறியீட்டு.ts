f9e9c116cbd7a2270252006622a104b35b214b00
julienmalard <julien.malard@mail.mcgill.ca>
2023-02-03 15:16:38 +0100

    version

--- மூலம்/குறியீட்டு.ts
+++ மூலம்/குறியீட்டு.ts
@@ -1,5 +1,5 @@
 export { கிளி } from "@/கிளி.js";
 export type { பரிந்துரை_வகை, பிணையம்_பரிந்துரை, அங்கீகரிக்கப்பட்ட_உறுப்படி_வகை } from "@/கிளி.js";
-export * from "@/மாறிலிகள்";
+export * from "@/மாறிலிகள்.js";
 
-export { version as பதிப்பு } from "@/பதிப்பு";
+export { version as பதிப்பு } from "@/பதிப்பு.js";
