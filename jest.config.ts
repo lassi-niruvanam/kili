557d7e291fcf2552b7e96c799c80bb13fd8164ef
julienmalard <julien.malard@mail.mcgill.ca>
2023-04-10 16:46:32 +0530

    வடிவூட்டம்

--- jest.config.ts
+++ jest.config.ts
@@ -1,23 +1,21 @@
-import type { JestConfigWithTsJest } from 'ts-jest'
+import type { JestConfigWithTsJest } from "ts-jest";
 
 const config: JestConfigWithTsJest = {
-    "transform": {
-      "(.*).ts$": [
-        "ts-jest",
-        {
-          "useESM": true
-        }
-      ]
-    },
-    "preset": "ts-jest/presets/default-esm",
-    "moduleNameMapper": {
-      "^@/(.*).js$": "<rootDir>/மூலம்/$1",
-      "^@/(.*)$": "<rootDir>/மூலம்/$1"
-    },
-    "testMatch": [
-      "**/*.சோதனை.ts"
+  transform: {
+    "(.*).ts$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
     ],
-    "testEnvironment": "node"
-  }
+  },
+  preset: "ts-jest/presets/default-esm",
+  moduleNameMapper: {
+    "^@/(.*).js$": "<rootDir>/மூலம்/$1",
+    "^@/(.*)$": "<rootDir>/மூலம்/$1",
+  },
+  testMatch: ["**/*.சோதனை.ts"],
+  testEnvironment: "node",
+};
 
-  export default config
+export default config;
