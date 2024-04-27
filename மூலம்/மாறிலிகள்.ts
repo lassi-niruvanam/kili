55ce9f51fe75b139b8391cb296bad1ffeb741a36
julienmalard <julien.malard@mail.mcgill.ca>
2024-04-27 20:43:09 +0200

    உண்மையான மாறிலிகள்

--- மூலம்/மாறிலிகள்.ts
+++ மூலம்/மாறிலிகள்.ts
@@ -2,11 +2,11 @@ export const அங்கீகார_தத_மீதரவு_சாபி = "
 
 export const பங்கேற்பாளர்_நெடுவரிசை_அடையாளம் = "பங்கேற்பாளர்";
 export const பங்கேற்பாளர்_மாறி_அடையாளம் =
-  "/orbitdb/zdpuAqBsFnWsRo4DRZjyvDbuHXPck4PsD99GqUqnDaVMYYdRy"; // மாற்றவும்
+  "/orbitdb/zdpuAunUaEc6CRX8wAjNixNioB6hF5SYvwJytx4ynm5wQx1vH";
 
 export const தேதி_நெடுவரிசை_அடையாளம் = "தேதி";
 export const தேதி_மாறி_அடையாளம் =
-  "/orbitdb/zdpuAtyjCoieAwksiuuvaYgVQfzFYSsB4isz5LuJqiiEnVdfS"; // மாற்றவும்
+  "/orbitdb/zdpuAkz4jAZW5EM3rK7huQoZAE6L26dtpoVd4UYGGyaQkDmTx";
 
 export const மாறிலிகள் = {
   அங்கீகார_தத_மீதரவு_சாபி,
