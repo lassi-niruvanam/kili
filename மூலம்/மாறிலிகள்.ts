3fe10fe561474be0c240953cfd82c700b5f67969
julienmalard <julien.malard@mail.mcgill.ca>
2025-05-14 17:23:19 +0530

    உண்மையான மாறி அடையாளங்கள்

--- மூலம்/மாறிலிகள்.ts
+++ மூலம்/மாறிலிகள்.ts
@@ -2,11 +2,11 @@ export const அங்கீகார_தத_மீதரவு_சாபி = "
 
 export const பங்கேற்பாளர்_நெடுவரிசை_அடையாளம் = "பங்கேற்பாளர்";
 export const பங்கேற்பாளர்_மாறி_அடையாளம் =
-  "/orbitdb/zdpuAunUaEc6CRX8wAjNixNioB6hF5SYvwJytx4ynm5wQx1vH";
+  "/orbitdb/zdpuAxqdx9cSPjsE7GpPFUfcvS6qZMEy6PddzzD6wwwW7kaPn";
 
 export const தேதி_நெடுவரிசை_அடையாளம் = "தேதி";
 export const தேதி_மாறி_அடையாளம் =
-  "/orbitdb/zdpuAkz4jAZW5EM3rK7huQoZAE6L26dtpoVd4UYGGyaQkDmTx";
+  "/orbitdb/zdpuAs73fYSmX7Z1ARkTMZqF3NLNRqsfjQZoU52PsWrCM9Ys6";
 
 export const மாறிலிகள் = {
   அங்கீகார_தத_மீதரவு_சாபி,
